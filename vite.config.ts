3bb5d69f192ae4126d6ce4211fc668ad1d19a5c5
Abdellah Khouden <khoudenak@gmail.com>
2026-01-02 23:59:31 +0100

    refactor Vite configuration to include server proxy settings for API requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://141.253.125.198:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
